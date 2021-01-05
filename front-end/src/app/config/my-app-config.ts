ab16b68ca21303748f09e9d8ab8b2d6c27f26d1a
woxdak <43390744+woxdak@users.noreply.github.com>
2021-01-04 21:43:52 -0600

    Add files via upload

---
+++ front-end/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3bzp7mQ0nJvrYn5d6',
+        issuer: 'https://dev-8956613.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
