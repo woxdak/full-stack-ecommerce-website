9ae2a16e34653619271d8ce6236a2d3af7a96593
woxdak <43390744+woxdak@users.noreply.github.com>
2021-01-04 21:46:18 -0600

    Add files via upload

---
+++ front-end/src/app/product-details/product-details.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartItem } from '../common/cart-item';
+import { Products } from '../common/products';
+import { CartService } from '../services/cart.service';
+import { ProductsService } from '../services/products.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Products = new Products();
+
+  constructor(private productService: ProductsService,
+    private cartService : CartService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  handleProductDetails() {
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
+
+
+  addToCart(){
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+  }
+}
