12a4078a62f89e5c0c09c9ae22233c93205d65d7
woxdak <43390744+woxdak@users.noreply.github.com>
2021-01-04 21:45:25 -0600

    Add files via upload

---
+++ front-end/src/app/product-category-menu/product-category-menu.component.ts
@@ -0,0 +1,31 @@
+
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-product-category-menu',
+  templateUrl: './product-category-menu.component.html',
+  styleUrls: ['./product-category-menu.component.css']
+})
+export class ProductCategoryMenuComponent implements OnInit {
+
+  productCategories: ProductCategory[];
+  
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit() {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+
+    this.productService.getProductCategories().subscribe(
+      data => {
+        console.log('Product Categories=' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    );
+  }
+
+}
