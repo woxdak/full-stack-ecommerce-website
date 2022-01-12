f8a5cba6677db2684d3fb9518f2fe75b7bb1804a
woxdak <43390744+woxdak@users.noreply.github.com>
2022-01-12 17:20:11 -0600

    Add files via upload

---
+++ front-end/src/app/components/components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: './components.component.html',
+  styleUrls: ['./components.component.css']
+})
+export class ComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
